418f5bf9e1906bbbc965619e3a983ce656179615
fuzzybabybunny <victor@victorlinphoto.com>
2014-08-16 21:01:42 +0800

    added functions to add pets and users

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,31 +1,35 @@
 Template.index.rendered = function(){
 
-  var addSelectedBy = function(personId, petId){
+  var addSelectedBy = function(userId, petId){
     var selectedByArray = Adoptees.findOne(petId).selectedBy;
     console.log(selectedByArray);
-    if(_.indexOf(selectedByArray, personId, true) == -1){
+    if(_.indexOf(selectedByArray, userId, true) == -1){
       Adoptees.update(petId, {$push: {"selectedBy": personId}} );
     };
   }
 
-  var addPetsSelected = function(personId, petId){
-    var petsSelectedArray = Users.findOne(personId).petsSelected;
+  var addPetsSelected = function(userId, petId){
+    var petsSelectedArray = Users.findOne(userId).petsSelected;
     console.log(petsSelectedArray);
     if(_.indexOf(petsSelectedArray, petId, true) == -1){
-      Users.update(personId, {$push: {"petsSelected": petId}} );
+      Users.update(userId, {$push: {"petsSelected": petId}} );
     };
   }
-  
+
   $('a[href*=#]').click(function(){return false;});
 
   var animationEndEvent = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
 
-  var Person = {
-    wrap: $('#people'),
-    people: Adoptees.find().fetch(),
+  var Pet = {
+    wrap: $('#pets'),
+    pets: [
+      {name: 'Linda', age: 25, img: 'img/dog1.jpeg'},
+      {name: 'Chuck', age: 25, img: 'http://funnydogspictures.org/wp-content/uploads/2013/01/betman-funny-dog-picture.jpg'},
+      {name: 'blah', age: 25, img: 'img/dog2.jpg'}
+    ],
     add: function(){
-      var random = this.people[Math.floor(Math.random() * this.people.length)];
-      $('#people').append("<div class='person'><img alt='" + random.name + "' src='" + random.img_url + "' /><span><strong>" + random.name + "</strong>, " + random.age + "</span></div>");
+      var random = this.pets[Math.floor(Math.random() * this.pets.length)];
+      $('#pets').append("<div class='pet'><img alt='" + random.name + "' src='" + random.img + "' /><span><strong>" + random.name + "</strong>, " + random.age + "</span></div>");
     }
   };
 
@@ -38,10 +42,10 @@ Template.index.rendered = function(){
       var self = this;
       if(!this.blocked){
         this.blocked = true;
-          $('.person').eq(0).addClass(animate).one(animationEndEvent, function(){
+          $('.pet').eq(0).addClass(animate).one(animationEndEvent, function(){
             $('.animateYes').remove();
             $('.animateNo').remove();
-            Person.add();
+            Pet.add();
             self.blocked = false;
         });
       }
@@ -56,9 +60,9 @@ Template.index.rendered = function(){
     App.like(false);
   });
 
-    Person.add();
-    Person.add();
-    Person.add();
-    Person.add();
+    Pet.add();
+    Pet.add();
+    Pet.add();
+    Pet.add();
 
 }

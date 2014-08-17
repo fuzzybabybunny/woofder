6425793fd7cc2c932b36a685c402ccf853bb1111
fuzzybabybunny <victor@victorlinphoto.com>
2014-08-17 11:30:12 +0800

    clicking on nav menu item makes nav go away

--- client/includes/header.js
+++ client/includes/header.js
@@ -1,7 +1,13 @@
 Template.header.events({
+  
   'click #login-buttons-logout': function() {
     Meteor.logout(function() {
       loginButtonsSession.closeDropdown();
     });
+  },
+
+  'click .navbar-collapse': function() {
+    $('.navbar-collapse').removeClass('in');
   }
+
 });

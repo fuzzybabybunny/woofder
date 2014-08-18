004b0e5253ac608639c8b2f7e6a477ff9d98da22
Screen Goose <sarrieschaz@hotmail.com>
2014-08-18 17:01:18 +0800

    Added e-mail capability

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,31 +1,31 @@
-if (Adoptees.find().count() === 0) {
-
-	Adoptees.insert({
-		img_url: 'dog1.jpeg',
-		name: 'Brian',
-		age: '8',
-		selectedBy: ["1"]
-	});
-
-	Adoptees.insert({
-		img_url: 'http://img2.wikia.nocookie.net/__cb20110502123804/pixar/images/7/77/Toy_story_3_old_buster.png',
-		name: 'Buster',
-		age: '10',
-		selectedBy: []
-	});
-
-	Adoptees.insert({
-		img_url: 'http://doginstructions.com/wp-content/uploads/2014/07/courage-the-cowardly-dogdeviantart--more-like-courage-the-cowardly-dog-by-butt-byu4hsic.jpg',
-		name: 'Courage',
-		age: '80',
-		selectedBy: []
-	});
-
-	Adoptees.insert({
-		img_url: 'http://img2.wikia.nocookie.net/__cb20140117110057/peanuts/images/4/46/Snoopy_and_Woodstock.png',
-		name: 'Snoopy',
-		age: '2',
-		selectedBy: []
-	});
-
-}
+// if (Adoptees.find().count() === 0) {
+//
+// 	Adoptees.insert({
+// 		img_url: 'dog1.jpeg',
+// 		name: 'Brian',
+// 		age: '8',
+// 		selectedBy: ["1"]
+// 	});
+//
+// 	Adoptees.insert({
+// 		img_url: 'http://img2.wikia.nocookie.net/__cb20110502123804/pixar/images/7/77/Toy_story_3_old_buster.png',
+// 		name: 'Buster',
+// 		age: '10',
+// 		selectedBy: []
+// 	});
+//
+// 	Adoptees.insert({
+// 		img_url: 'http://doginstructions.com/wp-content/uploads/2014/07/courage-the-cowardly-dogdeviantart--more-like-courage-the-cowardly-dog-by-butt-byu4hsic.jpg',
+// 		name: 'Courage',
+// 		age: '80',
+// 		selectedBy: []
+// 	});
+//
+// 	Adoptees.insert({
+// 		img_url: 'http://img2.wikia.nocookie.net/__cb20140117110057/peanuts/images/4/46/Snoopy_and_Woodstock.png',
+// 		name: 'Snoopy',
+// 		age: '2',
+// 		selectedBy: []
+// 	});
+//
+// }

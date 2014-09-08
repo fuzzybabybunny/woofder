eeab61450b690fce618fb5974aa23551d25073fe
fuzzybabybunny <victor@victorlinphoto.com>
2014-09-08 16:19:04 +0800

    Got drag and drop test working on /test - need to confirm on mobile

---
+++ client/application/layout.js
@@ -0,0 +1,8 @@
+Template.layout.created = function() {
+
+  $('head').append('<script type="text/javascript" src="/js/hammer.js"></script>');
+  $('head').append('<script type="text/javascript" src="/js/jquery-ui.js"></script>');
+  $('head').append('<script type="text/javascript" src="/js/jquery.hammer.js"></script>');
+
+};
+
--- client/pages/test.js
+++ client/pages/test.js
@@ -1,8 +1,20 @@
 Template.test.rendered = function(){
 
+	$(function() {
+	  $( "#draggable" ).draggable({ revert: "valid" });
+	  $( "#draggable2" ).draggable({ revert: "invalid" });
 
-
-
+	  $( "#droppable" ).droppable({
+	    activeClass: "ui-state-default",
+	    hoverClass: "ui-state-hover",
+	    drop: function( event, ui ) {
+	      $( this )
+	        .addClass( "ui-state-highlight" )
+	        .find( "p" )
+	          .html( "Dropped!" );
+	    }
+	  });
+	});
 
 };
 
